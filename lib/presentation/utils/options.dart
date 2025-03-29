82b56016e6f8970ca5b3473d860b5d78effd3f1e
Moisessantos45 <santosxphdz34@gmail.com>
2025-03-29 01:22:02 -0600

    change options.dart Fixed the Status Filter pop-up menu list where the name is in Spanish and the value is in English.

--- lib/presentation/utils/options.dart
+++ lib/presentation/utils/options.dart
@@ -3,10 +3,17 @@
 //   'Escolar',
 //   'Trabajo',
 //   'Casa',
+
 //   'Personal'
 // ];
 
 final List<String> statusOptions = ['Pending', 'In Progress', 'Completed'];
+final List<Map<String, String>> statusFilter = [
+  {'name': 'Pendiente', 'value': 'Pending'},
+  {'name': 'En Progreso', 'value': 'In Progress'},
+  {'name': 'Completo', 'value': 'Completed'},
+  {'name': 'Todos', 'value': 'All'},
+];
 List<String> listMenu = ['All', ...statusOptions];
 final List<String> priorityOptions = ['Low', 'Normal', 'High'];
 final List<int> statusColors = [0xFFE57373, 0xFF81C784, 0xFF64B5F6];
