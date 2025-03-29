ab1d1d6746faf255468f84c0a21441f6e85055bc
Moisessantos45 <santosxphdz34@gmail.com>
2025-03-29 12:27:33 -0600

    add app_colors.dart A class was added that extends color colors

--- lib/presentation/utils/app_colors.dart
+++ lib/presentation/utils/app_colors.dart
@@ -23,3 +23,39 @@ class AppColors {
   static const Color contentColorRed = Color(0xFFE80054);
   static const Color contentColorCyan = Color(0xFF50E4FF);
 }
+
+extension ColorExtension on Color {
+  Color darken([int percent = 40]) {
+    assert(1 <= percent && percent <= 100);
+    final value = 1 - percent / 100;
+    return Color.fromARGB(
+      _floatToInt8(a),
+      (_floatToInt8(r) * value).round(),
+      (_floatToInt8(g) * value).round(),
+      (_floatToInt8(b) * value).round(),
+    );
+  }
+
+  Color lighten([int percent = 40]) {
+    assert(1 <= percent && percent <= 100);
+    final value = percent / 100;
+    return Color.fromARGB(
+      _floatToInt8(a),
+      (_floatToInt8(r) + ((255 - _floatToInt8(r)) * value)).round(),
+      (_floatToInt8(g) + ((255 - _floatToInt8(g)) * value)).round(),
+      (_floatToInt8(b) + ((255 - _floatToInt8(b)) * value)).round(),
+    );
+  }
+
+  Color avg(Color other) {
+    final red = (_floatToInt8(r) + _floatToInt8(other.r)) ~/ 2;
+    final green = (_floatToInt8(g) + _floatToInt8(other.g)) ~/ 2;
+    final blue = (_floatToInt8(b) + _floatToInt8(other.b)) ~/ 2;
+    final alpha = (_floatToInt8(a) + _floatToInt8(other.a)) ~/ 2;
+    return Color.fromARGB(alpha, red, green, blue);
+  }
+
+  int _floatToInt8(double x) {
+    return (x * 255.0).round() & 0xff;
+  }
+}
