e040f191db50701619cd17f74308d8c6ce2e10c0
Moisessantos45 <santosxphdz34@gmail.com>
2025-03-29 11:55:05 -0600

    change navigation_bar.dart the design and distribution of items was changed

--- lib/presentation/widgets/navigation_bar.dart
+++ lib/presentation/widgets/navigation_bar.dart
@@ -6,44 +6,46 @@ class CustomNavigation extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withAlpha(20),
-            blurRadius: 10,
-            offset: const Offset(0, -2),
-          ),
-        ],
-      ),
-      child: BottomNavigationBar(
-        onTap: (index) {
-          if (index == 0) {
-            context.go("/");
-          } else if (index == 1) {
-            context.go("/profile");
-          }
-        },
+    return BottomAppBar(
         elevation: 0,
-        backgroundColor: Colors.white,
-        selectedItemColor: const Color(0xFF3498DB),
-        unselectedItemColor: Colors.grey[400],
-        selectedFontSize: 12,
-        unselectedFontSize: 12,
-        type: BottomNavigationBarType.fixed,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home_outlined, size: 28),
-            activeIcon: Icon(Icons.home, size: 28),
-            label: 'Inicio',
+        shape: const CircularNotchedRectangle(),
+        shadowColor: const Color(0xFF3498DB),
+        surfaceTintColor: Colors.grey[400],
+        color: Colors.white,
+        child: Container(
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withAlpha(15),
+                blurRadius: 10,
+                offset: const Offset(0, -2),
+              ),
+            ],
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person_outline, size: 28),
-            activeIcon: Icon(Icons.person, size: 28),
-            label: 'Perfil',
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              IconButton(
+                icon: const Icon(Icons.home),
+                onPressed: () => context.go("/"),
+              ),
+              IconButton(
+                icon: const Icon(Icons.stacked_bar_chart),
+                onPressed: () {
+                  context.go("/statistics");
+                },
+              ),
+              const SizedBox(width: 40),
+              IconButton(
+                icon: const Icon(Icons.notifications),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: const Icon(Icons.person),
+                onPressed: () => context.go("/profile"),
+              ),
+            ],
           ),
-        ],
-      ),
-    );
+        ));
   }
 }
