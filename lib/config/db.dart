fcc6be0fb64be14241395e3e7da4c83885ff8d10
Moisessantos45 <santosxphdz34@gmail.com>
2025-03-31 13:12:28 -0600

    change db.dart The hour, minute, avatar and active notification fields were added to the users table.

--- lib/config/db.dart
+++ lib/config/db.dart
@@ -13,7 +13,7 @@ Future<Database> initializeDatabase() async {
             'CREATE TABLE IF NOT EXISTS category (id TEXT PRIMARY KEY, name TEXT NOT NULL)');
 
         await db.execute(
-            'CREATE TABLE IF NOT EXISTS user (id TEXT PRIMARY KEY, name TEXT NOT NULL, nickname TEXT NOT NULL, level INTEGER NOT NULL)');
+            'CREATE TABLE IF NOT EXISTS user (id TEXT PRIMARY KEY, name TEXT NOT NULL, nickname TEXT NOT NULL, level INTEGER NOT NULL,hora INTEGER DEFAULT 0,minuto INTEGER DEFAULT 0,active_notifications TEXT DEFAULT "false"),avatar TEXT');
 
         await db.execute(
             'CREATE TABLE IF NOT EXISTS grade (id TEXT PRIMARY KEY, title TEXT NOT NULL, content TEXT, date TEXT, due_date TEXT, status TEXT, priority TEXT, category TEXT, color TEXT, point INTEGER)');
@@ -26,7 +26,7 @@ Future<Database> initializeDatabase() async {
             'CREATE TABLE IF NOT EXISTS category (id TEXT PRIMARY KEY, name TEXT NOT NULL)');
 
         await db.execute(
-            'CREATE TABLE IF NOT EXISTS user (id TEXT PRIMARY KEY, name TEXT NOT NULL, nickname TEXT NOT NULL, level INTEGER NOT NULL)');
+            'CREATE TABLE IF NOT EXISTS user (id TEXT PRIMARY KEY, name TEXT NOT NULL, nickname TEXT NOT NULL, level INTEGER NOT NULL,hora INTEGER DEFAULT 0,minuto INTEGER DEFAULT 0,active_notifications TEXT DEFAULT "false"),avatar TEXT');
 
         await db.execute(
             'CREATE TABLE IF NOT EXISTS grade (id TEXT PRIMARY KEY, title TEXT NOT NULL, content TEXT, date TEXT, due_date TEXT, status TEXT, priority TEXT, category TEXT, color TEXT, point INTEGER)');
@@ -38,7 +38,6 @@ Future<Database> initializeDatabase() async {
 
     return database;
   } catch (e) {
-    print(e);
     throw Exception(e);
   }
 }
