9225f84f574bf650b7ca7a8ba195cfe72f89a8b2
Moisessantos45 <santosxphdz34@gmail.com>
2025-03-31 14:19:43 -0600

    add entity_user.dart Added missing properties (hour, minute, avatar, active notifications)

--- lib/domains/entity_user.dart
+++ lib/domains/entity_user.dart
@@ -3,11 +3,19 @@ class EntityUser {
   final String name;
   final String nickname;
   final int level;
+  final int hora;
+  final int minuto;
+  final bool activeNotifications;
+  final String avatar;
 
   EntityUser({
     required this.id,
     required this.name,
     required this.nickname,
     this.level = 1,
+    this.hora = 0,
+    this.minuto = 0,
+    this.activeNotifications = false,
+    this.avatar = '92574792835600d793',
   });
 }
