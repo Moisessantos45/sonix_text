c9f3fdad95608d278b6e6a394797e6a6dc0eb857
Moisessantos45 <santosxphdz34@gmail.com>
2025-03-31 14:18:28 -0600

    resolve db.dart The user table was fixed as it had the avatar property outside the table.

--- lib/config/db.dart
+++ lib/config/db.dart
@@ -13,7 +13,7 @@ Future<Database> initializeDatabase() async {
             'CREATE TABLE IF NOT EXISTS category (id TEXT PRIMARY KEY, name TEXT NOT NULL)');
 
         await db.execute(
-            'CREATE TABLE IF NOT EXISTS user (id TEXT PRIMARY KEY, name TEXT NOT NULL, nickname TEXT NOT NULL, level INTEGER NOT NULL,hora INTEGER DEFAULT 0,minuto INTEGER DEFAULT 0,active_notifications TEXT DEFAULT "false"),avatar TEXT');
+            'CREATE TABLE IF NOT EXISTS user (id TEXT PRIMARY KEY, name TEXT NOT NULL, nickname TEXT NOT NULL, level INTEGER NOT NULL,hora INTEGER DEFAULT 0,minuto INTEGER DEFAULT 0,active_notifications TEXT DEFAULT "false",avatar TEXT DEFAULT "92574792835600d793")');
 
         await db.execute(
             'CREATE TABLE IF NOT EXISTS grade (id TEXT PRIMARY KEY, title TEXT NOT NULL, content TEXT, date TEXT, due_date TEXT, status TEXT, priority TEXT, category TEXT, color TEXT, point INTEGER)');
@@ -26,7 +26,7 @@ Future<Database> initializeDatabase() async {
             'CREATE TABLE IF NOT EXISTS category (id TEXT PRIMARY KEY, name TEXT NOT NULL)');
 
         await db.execute(
-            'CREATE TABLE IF NOT EXISTS user (id TEXT PRIMARY KEY, name TEXT NOT NULL, nickname TEXT NOT NULL, level INTEGER NOT NULL,hora INTEGER DEFAULT 0,minuto INTEGER DEFAULT 0,active_notifications TEXT DEFAULT "false"),avatar TEXT');
+            'CREATE TABLE IF NOT EXISTS user (id TEXT PRIMARY KEY, name TEXT NOT NULL, nickname TEXT NOT NULL, level INTEGER NOT NULL,hora INTEGER DEFAULT 0,minuto INTEGER DEFAULT 0,active_notifications TEXT DEFAULT "false",avatar TEXT DEFAULT "92574792835600d793")');
 
         await db.execute(
             'CREATE TABLE IF NOT EXISTS grade (id TEXT PRIMARY KEY, title TEXT NOT NULL, content TEXT, date TEXT, due_date TEXT, status TEXT, priority TEXT, category TEXT, color TEXT, point INTEGER)');
