3bfecd059f2774635d8bb0597da7438e18fc40f4
Moisessantos45 <santosxphdz34@gmail.com>
2025-03-31 22:35:53 -0600

    change validate_string.dart Fixed regex to allow accents

--- lib/presentation/utils/validate_string.dart
+++ lib/presentation/utils/validate_string.dart
@@ -1,5 +1,6 @@
 bool isValidString(String cadena) {
-  final RegExp regex = RegExp(r'^[a-zA-Z0-9]+( [a-zA-Z0-9]+)*$');
+  final RegExp regex =
+      RegExp(r'^[a-zA-Z0-9áéíóúüÁÉÍÓÚÜñÑ]+( [a-zA-Z0-9áéíóúüÁÉÍÓÚÜñÑ]+)*$');
 
   return cadena.isNotEmpty && cadena.trim() == cadena && regex.hasMatch(cadena);
 }
