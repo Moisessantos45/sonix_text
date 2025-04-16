01027ab075d39436c8d8e3cda5ef1c0f19a2ed4c
Moisessantos45 <santosxphdz34@gmail.com>
2025-04-16 00:04:41 -0600

    add options_grade.dart The option to select a color was added

--- lib/presentation/widgets/options_grade.dart
+++ lib/presentation/widgets/options_grade.dart
@@ -2,28 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:sonix_text/presentation/riverpod/repository_category.dart';
 import 'package:sonix_text/presentation/utils/options.dart';
+import 'package:sonix_text/presentation/widgets/color_select.dart';
 
 class GradeOptionsWidget extends ConsumerStatefulWidget {
   final TextEditingController category;
   final TextEditingController status;
   final TextEditingController priority;
   final TextEditingController dueDate;
+  final ValueChanged<int> onTapColor;
 
   const GradeOptionsWidget(
       {super.key,
       required this.category,
       required this.status,
       required this.priority,
-      required this.dueDate});
+      required this.dueDate,
+      required this.onTapColor});
 
   @override
   ConsumerState<GradeOptionsWidget> createState() => _GradeOptionsWidgetState();
 }
 
 class _GradeOptionsWidgetState extends ConsumerState<GradeOptionsWidget> {
-  // String _selectedCategory = 'General';
-  // String _selectedStatus = 'Pending';
-  // String _selectedPriority = 'Normal';
   DateTime _dueDate = DateTime.now().add(const Duration(days: 1));
   bool isInit = false;
 
@@ -107,7 +107,6 @@ class _GradeOptionsWidgetState extends ConsumerState<GradeOptionsWidget> {
                           onChanged: (value) {
                             if (value != null) {
                               setState(() {
-                                // _selectedCategory = value;
                                 widget.category.text = value;
                               });
                             }
@@ -134,7 +133,6 @@ class _GradeOptionsWidgetState extends ConsumerState<GradeOptionsWidget> {
                         onChanged: (value) {
                           if (value != null) {
                             setState(() {
-                              // _selectedStatus = value;
                               widget.status.text = value;
                             });
                           }
@@ -151,7 +149,6 @@ class _GradeOptionsWidgetState extends ConsumerState<GradeOptionsWidget> {
                         onChanged: (value) {
                           if (value != null) {
                             setState(() {
-                              // _selectedPriority = value;
                               widget.priority.text = value;
                             });
                           }
@@ -160,7 +157,19 @@ class _GradeOptionsWidgetState extends ConsumerState<GradeOptionsWidget> {
                     ),
                   ],
                 ),
-              )
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(vertical: 8),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: ColorSelector(onColorSelected: (color) {
+                        widget.onTapColor(color);
+                      }),
+                    ),
+                  ],
+                ),
+              ),
             ],
           );
   }
@@ -178,14 +187,12 @@ class _GradeOptionsWidgetState extends ConsumerState<GradeOptionsWidget> {
 
     return Container(
       decoration: BoxDecoration(
-        color: const Color(0xFFF9FAFB),
-        borderRadius: BorderRadius.circular(10),
-        border: Border.all(color: const Color(0xFFECF0F1)),
+        color: Colors.white,
       ),
       padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
       child: Row(
         children: [
-          Icon(icon, size: 18, color: const Color(0xFF95A5A6)),
+          Icon(icon, size: 18, color: Colors.greenAccent),
           const SizedBox(width: 8),
           Expanded(
             child: DropdownButtonHideUnderline(
@@ -193,8 +200,8 @@ class _GradeOptionsWidgetState extends ConsumerState<GradeOptionsWidget> {
                 value: validValue,
                 isDense: true,
                 isExpanded: true,
-                icon:
-                    const Icon(Icons.arrow_drop_down, color: Color(0xFF95A5A6)),
+                icon: const Icon(Icons.arrow_drop_down,
+                    color: Colors.greenAccent),
                 style: const TextStyle(
                   fontSize: 14,
                   color: Color(0xFF2C3E50),
@@ -219,16 +226,12 @@ class _GradeOptionsWidgetState extends ConsumerState<GradeOptionsWidget> {
     return InkWell(
       onTap: () => _selectDate(context),
       child: Container(
-        decoration: BoxDecoration(
-          color: const Color(0xFFF9FAFB),
-          borderRadius: BorderRadius.circular(10),
-          border: Border.all(color: const Color(0xFFECF0F1)),
-        ),
+        decoration: BoxDecoration(color: Colors.white),
         padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
         child: Row(
           children: [
             const Icon(Icons.calendar_today,
-                size: 18, color: Color(0xFF95A5A6)),
+                size: 18, color: Colors.greenAccent),
             const SizedBox(width: 8),
             Expanded(
               child: Text(
