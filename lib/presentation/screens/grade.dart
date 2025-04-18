b1ef2a0600f9466ab974fa1b7c17660779fd9595
Moisessantos45 <santosxphdz34@gmail.com>
2025-04-17 23:39:27 -0600

    change grade.dart A calendar has been integrated at the top to view notes by date.

--- lib/presentation/screens/grade.dart
+++ lib/presentation/screens/grade.dart
@@ -1,3 +1,4 @@
+import 'package:calendar_timeline/calendar_timeline.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:sonix_text/presentation/riverpod/repository_grade.dart';
@@ -19,9 +20,19 @@ class _GradeScreenState extends ConsumerState<GradeScreen> {
     return 0.2126 * r + 0.7152 * g + 0.0722 * b;
   }
 
+  String subStringContent(String content) {
+    if (content.length > 50) {
+      return '${content.substring(0, 50)}...';
+    } else {
+      return content;
+    }
+  }
+
+  DateTime selectedDate = DateTime.now();
+
   @override
   Widget build(BuildContext context) {
-    final grades = ref.watch(allGradesProvider);
+    final grades = ref.watch(gradesFilterDateProvider);
 
     return Scaffold(
       backgroundColor: Colors.white,
@@ -38,52 +49,89 @@ class _GradeScreenState extends ConsumerState<GradeScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: MasonryGridView.count(
-          crossAxisCount: 2,
-          mainAxisSpacing: 8.0,
-          crossAxisSpacing: 8.0,
-          itemCount: grades.length,
-          itemBuilder: (context, index) {
-            final grade = grades[index];
-            final backgroundColor = Color(grade.color);
-            final luminosity = calculateLuminosity(backgroundColor);
-            final textColor = luminosity > 0.5 ? Colors.black87 : Colors.white;
-            return Container(
-              padding: const EdgeInsets.all(8.0),
-              decoration: BoxDecoration(
-                color: backgroundColor,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    grade.title,
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                      color: textColor,
+        child: Column(
+          children: [
+            CalendarTimeline(
+              initialDate: selectedDate,
+              firstDate: DateTime.now().subtract(const Duration(days: 30)),
+              lastDate: DateTime.now().add(const Duration(days: 365 * 10)),
+              onDateSelected: (date) {
+                setState(() {
+                  selectedDate = date;
+                });
+                ref
+                    .watch(gradeFilterDateNotifierProvider.notifier)
+                    .setDate(date);
+              },
+              leftMargin: 20,
+              monthColor: Colors.blueGrey,
+              dayColor: Colors.teal[200],
+              activeDayColor: Colors.white,
+              activeBackgroundDayColor: Colors.redAccent[100],
+              selectableDayPredicate: (date) => date.day != 23,
+              locale: 'en_ISO',
+            ),
+            const SizedBox(height: 15),
+            grades.isEmpty
+                ? const Center(
+                    child: Text(
+                      'No hay notas para mostrar',
+                      style: TextStyle(fontSize: 16, color: Colors.grey),
                     ),
-                  ),
-                  const SizedBox(height: 8),
-                  Text(
-                    grade.date,
-                    style: TextStyle(
-                      fontSize: 14,
-                      color: textColor.withAlpha(700),
-                    ),
-                  ),
-                  const SizedBox(height: 8),
-                  Text(
-                    grade.content,
-                    style: TextStyle(fontSize: 14, color: textColor),
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 5,
-                  ),
-                ],
-              ),
-            );
-          },
+                  )
+                : Expanded(
+                    child: MasonryGridView.count(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 8.0,
+                    crossAxisSpacing: 8.0,
+                    itemCount: grades.length,
+                    physics: const BouncingScrollPhysics(),
+                    itemBuilder: (context, index) {
+                      final grade = grades[index];
+                      final backgroundColor = Color(grade.color);
+                      final luminosity = calculateLuminosity(backgroundColor);
+                      final textColor =
+                          luminosity > 0.5 ? Colors.black87 : Colors.white;
+
+                      final content = subStringContent(grade.content);
+                      return Container(
+                        padding: const EdgeInsets.all(8.0),
+                        decoration: BoxDecoration(
+                          color: backgroundColor,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              grade.title,
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: textColor,
+                              ),
+                            ),
+                            const SizedBox(height: 8),
+                            Text(
+                              grade.date,
+                              style: TextStyle(
+                                fontSize: 14,
+                                color: textColor.withAlpha(700),
+                              ),
+                            ),
+                            const SizedBox(height: 8),
+                            Text(
+                              content,
+                              style: TextStyle(fontSize: 14, color: textColor),
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 5,
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ))
+          ],
         ),
       ),
     );
