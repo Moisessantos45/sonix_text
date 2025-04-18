3142f92a6a0899bd289f7f3357797fc9568d11db
Moisessantos45 <santosxphdz34@gmail.com>
2025-04-18 02:54:36 -0600

    add shared_preferents.dart Added methods for saving app versions

--- lib/config/helper/shared_preferents.dart
+++ lib/config/helper/shared_preferents.dart
@@ -15,10 +15,28 @@ class SharedPreferentsManager {
     }
   }
 
+  Future<String> getCodeVersion(String key) async {
+    try {
+      final pref = await SharedPreferences.getInstance();
+      return pref.getString(key) ?? "0.0.0";
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+
   Future<void> saveData(String key, bool value) async {
     try {
       final pref = await SharedPreferences.getInstance();
       await pref.setBool(key, value);
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+
+  Future<void> saveCodeVersion(String key, String value) async {
+    try {
+      final pref = await SharedPreferences.getInstance();
+      await pref.setString(key, value);
     } catch (e) {
       throw Exception(e);
     }
