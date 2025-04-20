a11c037f66adadb8f67375d4f00de58042bb8486
Moisessantos45 <santosxphdz34@gmail.com>
2025-04-20 00:25:42 -0600

    Change page_router.dart to update the page transition to ScaleTransition and adjust the transition duration.

--- lib/config/helper/page_router.dart
+++ lib/config/helper/page_router.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 
-class CustomPageRoute extends PageRouteBuilder{
+class CustomPageRoute extends PageRouteBuilder {
   final Widget page;
-  CustomPageRoute({required this.page}) : super(
-    pageBuilder: (context, animation, secondaryAnimation) => page,
-    transitionsBuilder: (context, animation, secondaryAnimation, child) {
-      return FadeTransition(
-        opacity: animation,
-        child: child,
-      );
-    },
-    transitionDuration: const Duration(milliseconds: 300),
-  );
+
+  CustomPageRoute({required this.page})
+      : super(
+          pageBuilder: (context, animation, secondaryAnimation) => page,
+          transitionDuration: const Duration(milliseconds: 400),
+          reverseTransitionDuration: Duration.zero,
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            return ScaleTransition(
+              scale: animation,
+              child: child,
+            );
+          },
+        );
 }
