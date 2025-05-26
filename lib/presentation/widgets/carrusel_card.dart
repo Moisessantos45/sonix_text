8c5d82e99b099597d36ed63e563ec176c08906d0
Moisessantos45 <santosxphdz34@gmail.com>
2025-05-26 10:47:29 -0600

    remove carrusel_card.dart Remove unnecessary go_router import in carousel_card.dart

--- lib/presentation/widgets/carrusel_card.dart
+++ lib/presentation/widgets/carrusel_card.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 import 'package:sonix_text/presentation/riverpod/repository_grade.dart';
 
 class CarouselCard extends ConsumerWidget {
