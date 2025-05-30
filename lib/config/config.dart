4f66617b1317c73e8365014d505dc4934969e0ab
Moisessantos45 <santosxphdz34@gmail.com>
2025-05-30 04:47:06 -0600

    Add config.dart file to export service, database, and helper configurations

---
+++ lib/config/config.dart
@@ -0,0 +1,5 @@
+export 'package:sonix_text/config/service/notifications.dart';
+export 'package:sonix_text/config/db.dart';
+export 'package:sonix_text/config/router/router.dart';
+export 'package:sonix_text/config/helper/page_router.dart';
+export 'package:sonix_text/config/helper/shared_preferents.dart';
