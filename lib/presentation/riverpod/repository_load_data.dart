64fad7fcd17c233864c9539a59bfb71d9abbbade
Moisessantos45 <santosxphdz34@gmail.com>
2025-05-30 04:49:28 -0600

    Add LoadDataNotifier and provider to manage loading of data for levels, users, grades, and categories

---
+++ lib/presentation/riverpod/repository_load_data.dart
@@ -0,0 +1,36 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sonix_text/presentation/riverpod/riverpod.dart';
+
+final loadDataProvider =
+    StateNotifierProvider<LoadDataNotifier, List<dynamic>>((ref) {
+  final level = ref.watch(levelNotifierProvider.notifier);
+  final users = ref.watch(userNotifierProvider.notifier);
+  final grades = ref.watch(allGradesProvider.notifier);
+  final categories = ref.watch(categoryNotifierProvider.notifier);
+  return LoadDataNotifier(level, users, grades, categories);
+});
+
+class LoadDataNotifier extends StateNotifier<List<dynamic>> {
+  final LevelNotifier _levelNotifier;
+  final UserNotifier _userNotifier;
+  final AllGradesNotifier _gradeNotifier;
+  final CategoryNotifier _categoryNotifier;
+
+  LoadDataNotifier(this._levelNotifier, this._userNotifier, this._gradeNotifier,
+      this._categoryNotifier)
+      : super([]);
+
+  Future<void> loadData() async {
+    try {
+      await Future.wait([
+        _levelNotifier.getLevels(),
+        _userNotifier.getUsers(),
+        _gradeNotifier.loadGrades(),
+        _categoryNotifier.getCategories(),
+      ]);
+      state = [];
+    } catch (e) {
+      state = [];
+    }
+  }
+}
