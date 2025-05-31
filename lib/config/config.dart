2cda519b56e097e92671346d82031b3a3dd43955
Moisessantos45 <santosxphdz34@gmail.com>
2025-05-31 04:58:42 -0600

    Add API class export in config.dart

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -3,3 +3,4 @@ export 'package:sonix_text/config/db.dart';
 export 'package:sonix_text/config/router/router.dart';
 export 'package:sonix_text/config/helper/page_router.dart';
 export 'package:sonix_text/config/helper/shared_preferents.dart';
+export 'package:sonix_text/config/service/api/api.dart';
