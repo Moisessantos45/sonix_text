d0cf5c38da7c047aef049589303e8167a819bcf5
Moisessantos45 <santosxphdz34@gmail.com>
2025-05-31 14:25:58 -0600

    Add version checking and update notification to the API

--- lib/config/service/api/api.dart
+++ lib/config/service/api/api.dart
@@ -1,4 +1,9 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:sonix_text/config/config.dart';
 import 'package:sonix_text/config/helper/shared_preferents.dart';
+import 'package:sonix_text/presentation/utils/generate_id.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class VersionApi {
   final SharedPreferentsManager _sharedPreferents;
@@ -37,5 +42,35 @@ class VersionApi {
     } catch (e) {
       throw Exception(e);
     }
+  }
+}
+
+Future<void> checkVersion(VersionApi versionApi) async {
+  try {
+    final versionCode = await versionApi.getVersionDevice();
+    final dio = Dio();
+
+    final response = await dio.get(
+      '${dotenv.env['API_URL']}/version-check',
+      queryParameters: {
+        'app': 'sonix_text-$versionCode',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      final data = response.data;
+      if (data['data'] != null) {
+        await NotificationsService.showUpdateNotification(
+          id: generateUniqueId(),
+          title: "Actualización disponible",
+          body:
+              'Una nueva versión de Sonix Text está disponible. Por favor, actualiza la aplicación.',
+          scheduleDate: DateTime.now().add(const Duration(seconds: 5)),
+          version: data['data']['codeVersion'],
+        );
+      }
+    }
+  } catch (e) {
+    debugPrint('Error al verificar la versión: $e');
   }
 }
