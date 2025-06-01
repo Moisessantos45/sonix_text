dc96809145b83f07d02a01cd294c816168aaeb49
Moisessantos45 <santosxphdz34@gmail.com>
2025-06-01 15:50:02 -0600

    Adding the EntityNoteLinks class to manage note links

---
+++ lib/domains/entity_note_links.dart
@@ -0,0 +1,13 @@
+class EntityNoteLinks {
+  final int id;
+  final String fromId;
+  final String toId;
+  final String createdAt;
+
+  EntityNoteLinks({
+    required this.id,
+    required this.fromId,
+    required this.toId,
+    required this.createdAt,
+  });
+}
